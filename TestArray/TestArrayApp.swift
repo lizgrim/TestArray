48cfc008c3cddc4ac2cd54afee689ba5da97565b
Liz Grim <liz.grimley@gmail.com>
2021-03-30 06:35:17 +1100

    Initial Commit

---
+++ TestArray/TestArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestArrayApp.swift
+//  TestArray
+//
+//  Created by Liz on 30/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TestArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
